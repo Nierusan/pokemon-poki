6586b2da2aa97b47989dd587fc5be1f614a55718
Nielle <ythevoz@outlook.com>
2024-12-24 12:31:47 +0100

    Initiate Pokemon app - react native

---
+++ app/pokemon/[id].tsx
@@ -0,0 +1,8 @@
+import {Text, View} from "react-native";
+
+export default function Pokemon() {
+    const params = {useLocalSearchParams}
+    return <View>
+        <Text>Pokemon {params.id}</Text>
+    </View>
+}
